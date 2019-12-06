72e465c8ae8ed62e439c9d60bcaf5042f57c4c91
dmitrij <dimasek28@gmail.com>
2019-12-06 21:30:44 +0300

    45

--- sources/broker.cpp
+++ sources/broker.cpp
@@ -42,8 +42,7 @@ void DirectoryAnalyzer::is_laster
         data._new = _new_p;
         data._new = _new_p;
     }
-    else if ( _new_date[1] == data.date[1] ||_new_date[2] > data.date[2] )
-    {
+    else if ( _new_date[1] == data.date[1] ||_new_date[2] > data.date[2] ){
         data.date[0] = _new_date[0];
         data.date[1] = _new_date[1];
         data.date[2] = _new_date[2];
@@ -53,8 +52,7 @@ void DirectoryAnalyzer::is_laster
         data._new = _new_p;
     }
     else if ( _new_date[1] == data.date[1] || _new_date[2] == data.date[2]
-    || _new_date[3] > data.date[3] )
-    {
+    || _new_date[3] > data.date[3] ){
         data.date[0] = _new_date[0];
         data.date[1] = _new_date[1];
         data.date[2] = _new_date[2];
