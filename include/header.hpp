d3fffb71a5dfcf19d060cc75545ddc75173225ba
dmitrij <dimasek28@gmail.com>
2019-12-06 23:24:42 +0300

    5555

--- include/header.hpp
+++ include/header.hpp
@@ -2,7 +2,7 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 #include "vector"
-#define condition = _new_date[1] == data.date[1] || _new_date[2] == data.date[2]|| _new_date[3] > data.date[3]
+
 struct FileInfo
 {
     boost::filesystem::path _new;//путь
