2909735637685fb8188bd67a93adfb9a6085364a
dmitrij <dimasek28@gmail.com>
2019-12-07 11:28:49 +0300

    5

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dmitrij/lab-04-boost-filesystem
-# Build directory: /Users/dmitrij/lab-04-boost-filesystem/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/Users/dmitrij/lab-04-boost-filesystem/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/dmitrij/lab-04-boost-filesystem/CMakeLists.txt;59;add_test;/Users/dmitrij/lab-04-boost-filesystem/CMakeLists.txt;0;")
